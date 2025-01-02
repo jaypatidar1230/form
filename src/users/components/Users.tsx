bd8580b22846625315d49ba8a43b0f33678be9b6
Jay <11a21230jay2019@gmail.com>
2025-01-02 19:01:50 +0530

    connect zod

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,26 +1,19 @@
 import { useForm } from 'react-hook-form';
-export function Users() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<{ email: string }>({ mode: 'all' });
 
-  const onSubmit = () => {
-    console.log("object")
-  }
+import { zodResolver } from '@hookform/resolvers/zod';
+import { schema, Schema } from '../types/schema';
+
+import { TextField } from '@mui/material';
+
+export function Users() {
+  const { register } = useForm<Schema>({
+    mode: 'all',
+    resolver: zodResolver(schema),
+  });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        className="m-2 border-blue-400 pl-2 border-2 rounded"
-        {...register('email', {
-          required: { value: true, message: 'The email is required' },
-          maxLength: { value: 10, message: 'To many characters' },
-        })}
-        placeholder="Email"
-      />
-      <p>{errors.email?.message}</p>
-    </form>
+    <>
+      <TextField {...register('name')} />
+    </>
   );
 }
--- src/users/types/schema.ts
+++ src/users/types/schema.ts
@@ -1,5 +1,14 @@
 import { z } from 'zod';
+import { patterns } from '../../constants';
 
 export const schema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(1, { message: 'Required' }),
+  email: z
+    .string()
+    .min(1, { message: 'Required' })
+    .refine((text) => patterns.email.test(text), {
+      message: 'Email is not valid',
+    }),
 });
+
+export type Schema = z.infer<typeof schema>;
