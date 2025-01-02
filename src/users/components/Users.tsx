34eabd6041c96b8ec91f10db6941bba43eb9ed89
Jay <11a21230jay2019@gmail.com>
2025-01-02 12:29:35 +0530

    form without zod

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,15 +1,26 @@
 import { useForm } from 'react-hook-form';
 export function Users() {
-  const { register } = useForm<{ email: string }>();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<{ email: string }>({ mode: 'all' });
+
+  const onSubmit = () => {
+    console.log("object")
+  }
 
   return (
-    <input
-      className="m-2 border-blue-400 pl-2 border-2 rounded"
-      {...register('email', {
-        required: { value: true, message: 'the email is required' },
-        maxLength: { value: 10, message: 'to many characters' },
-      })}
-      placeholder="Email"
-    />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        className="m-2 border-blue-400 pl-2 border-2 rounded"
+        {...register('email', {
+          required: { value: true, message: 'The email is required' },
+          maxLength: { value: 10, message: 'To many characters' },
+        })}
+        placeholder="Email"
+      />
+      <p>{errors.email?.message}</p>
+    </form>
   );
 }
