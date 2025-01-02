9d1b87a82830f19ed5323a6670e66c7cb7d0e0af
Jay <11a21230jay2019@gmail.com>
2025-01-02 11:49:50 +0530

    first commit

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,9 +1,15 @@
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form';
 export function Users() {
-    
-    const { register } = useForm<{ email : string }>();
+  const { register } = useForm<{ email: string }>();
 
   return (
-    <input {...register('email')}/>
-  )
+    <input
+      className="m-2 border-blue-400 pl-2 border-2 rounded"
+      {...register('email', {
+        required: { value: true, message: 'the email is required' },
+        maxLength: { value: 10, message: 'to many characters' },
+      })}
+      placeholder="Email"
+    />
+  );
 }
