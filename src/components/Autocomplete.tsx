b77fdcfdc717dc5d1693742ccd15a3e0c1b3133e
Jay <11a21230jay2019@gmail.com>
2025-01-03 18:40:58 +0530

    show error

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,6 @@
+import { Controller } from "react-hook-form";
+
+export function Autocomplete() {
+  return <Controller render={(params) => <Autocomplete />} />
+  
+}
--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -3,17 +3,29 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { schema, Schema } from '../types/schema';
 
-import { TextField } from '@mui/material';
+import { Stack, TextField } from '@mui/material';
 
 export function Users() {
-  const { register } = useForm<Schema>({
+  const {
+    register,
+    formState: { errors },
+  } = useForm<Schema>({
     mode: 'all',
     resolver: zodResolver(schema),
   });
 
   return (
-    <>
-      <TextField {...register('name')} />
-    </>
+    <div className="m-3">
+      <Stack sx={{ gap: 2 }}>
+        <TextField
+          {...register('name')}
+          label="Name"
+          error={!!errors.name}
+          helperText={errors.name?.message}
+        />
+        <TextField {...register('email')} label="Email" error={!!errors.email}
+          helperText={errors.email?.message}/>
+      </Stack>
+    </div>
   );
 }
